6ccafd5d58a887520d098b9d53c5c63e7f69bc44
jimmywea <phucbittuot@gmail.com>
2024-10-06 17:10:37 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-app.js";
-import { getFirestore, collection, addDoc, getDocs, query, where } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-firestore.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-firestore.js";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: "AIzaSyDL56ekmdndk3wd099KuJWUyogRUa3bwW8",
     authDomain: "kidstars-7434d.firebaseapp.com",
@@ -13,8 +11,5 @@ const firebaseConfig = {
     measurementId: "G-FJMK0F1LRN"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-export { db, collection, addDoc, getDocs, query, where };
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
