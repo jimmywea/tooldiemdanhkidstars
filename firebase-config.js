22eeef2e2417ad91208b8e222a1461ef3a9151b1
jimmywea <phucbittuot@gmail.com>
2024-10-06 16:04:56 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,17 +1,16 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-firestore.js";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDL56ekmdndk3wd099KuJWUyogRUa3bwW8",
-  authDomain: "kidstars-7434d.firebaseapp.com",
-  projectId: "kidstars-7434d",
-  storageBucket: "kidstars-7434d.appspot.com",
-  messagingSenderId: "616350873520",
-  appId: "1:616350873520:web:9d765d0bf5a483fa964875",
-  measurementId: "G-FJMK0F1LRN"
+    apiKey: "AIzaSyDL56ekmdndk3wd099KuJWUyogRUa3bwW8",
+    authDomain: "kidstars-7434d.firebaseapp.com",
+    projectId: "kidstars-7434d",
+    storageBucket: "kidstars-7434d.appspot.com",
+    messagingSenderId: "616350873520",
+    appId: "1:616350873520:web:9d765d0bf5a483fa964875",
+    measurementId: "G-FJMK0F1LRN"
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
