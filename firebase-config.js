4f3839281e0a011f4d4397b201fac611fa442d6d
jimmywea <phucbittuot@gmail.com>
2024-10-06 15:02:17 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,21 +1,17 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-firestore-compat.js";
+// Firebase Configuration File
+// Initialize Firebase using the compat library
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDL56ekmdndk3wd099KuJWUyogRUa3bwW8",
-  authDomain: "kidstars-7434d.firebaseapp.com",
-  projectId: "kidstars-7434d",
-  storageBucket: "kidstars-7434d.appspot.com",
-  messagingSenderId: "616350873520",
-  appId: "1:616350873520:web:9d765d0bf5a483fa964875",
-  measurementId: "G-FJMK0F1LRN"
+    apiKey: "AIzaSyDL56ekmdndk3wd099KuJWUyogRUa3bwW8",
+    authDomain: "kidstars-7434d.firebaseapp.com",
+    projectId: "kidstars-7434d",
+    storageBucket: "kidstars-7434d.appspot.com",
+    messagingSenderId: "616350873520",
+    appId: "1:616350873520:web:9d765d0bf5a483fa964875",
+    measurementId: "G-FJMK0F1LRN"
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-// Export database to be used in other files
-export { db };
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
